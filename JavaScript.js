d9b5d236318d7d70d11a6022fb4aa729856671ba
Matt Ito <mito626@gmail.com>
2013-03-17 20:36:54 -0700

    second commit

--- JavaScript.js
+++ JavaScript.js
@@ -1,16 +1,16 @@
 ﻿function enterNumber() {
 
-    var userNumber = prompt("Please enter a number between 2 and 10: ", " ")
+    var userNumber = prompt("Please enter a number between 2 and 10: ", "")
 
     if (userNumber >=2 && userNumber <=10) {
 
-        document.writeln("<h2>Your input number is " + userNumber + " </h2>")
+        document.writeln("<h2>Your input number is " + userNumber + " </h2>");
         var millionth = userNumber;
         for (var counter = 0; millionth > .000001 ; counter++) {
-            millionth /= 2;
+            millionth = millionth / 2;
         }
 
-        document.writeln("<h2>The number of times to divide the number " + userNumber + " by 2 to get a value less than one millionth is " + count + "</h2>")
+        document.writeln("<h2>The number of times to divide the number " + userNumber + " by 2 to get a value less than one millionth is " + count + "</h2>");
 
         var stars = userNumber;
         document.write("<p>");
@@ -28,6 +28,6 @@
     }
     else {
 
-        document.writeln("<h2>Your input is " + userNumber + ". The valid input number is between 2 and 10. Please reload this page and try again.")
+        document.writeln("<h2>Your input is " + userNumber + ". The valid input number is between 2 and 10. Please reload this page and try again.");
     }
 }
