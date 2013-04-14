fe94e8a7b2c73ee6a6c34c31b1e4202d051325b5
Matt Ito <mito626@gmail.com>
2013-04-13 21:53:18 -0700

    first commit

---
+++ QuestionOne.js
@@ -0,0 +1,33 @@
+﻿function enterNumber() {
+
+    var userNumber = prompt("Please enter a number between 5 and 20: ", "")
+
+    /*Check if number is between 2 and 10*/
+    if (userNumber >= 5 && userNumber <= 20) {
+        var i,j;	// loop variables
+        document.write("<table>");
+        document.write("<tr>");
+        for (var row = 1; row <= userNumber; row++) {
+            document.write(row);        
+        }
+        document.write("</br>");
+        document.write("</tr>");
+        for (j=1; j<=userNumber; j++) {
+            for (i = 1; i <= userNumber; i++) {
+
+                document.write("\t", i * j);
+                
+            }
+            document.write("</br>");	// end the line
+
+        }
+        document.write("</table>");
+    }
+
+        /*number is not within range */
+    else {
+        document.write("<span>");
+        document.writeln("<h2>The valid input number is between 5 and 20, please refresh this page and try again.</h2>");
+        document.write("</span>");
+    }
+}
---
+++ QuestionTwo.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    $("#run").click(function (event) {
+        var inputText = $("#inputText").val();
+        $("#output").text(inputText);
+        $("#message").show;
+        var usernameLength = $("#username").length();
+        var passwordLength = $("#password").length();
+        var conformationLength = $("#conformation").length();
+        if ((usernameLength < 4 && usernameLength > 8) || (passwordLength < 4 && passwordLength > 8) || (conformationLength < 4 && conformationLength > 8))
+        {
+            $("#message").text("The length of the user name and password must be between 4 and 8").css("color","red");
+        }
+        var passwordTest = $("#password").val();
+        var conformationTest = $("conformation").val();
+        if (passwordTest != conformationTest) {
+            $("message").text("The passwords are different").css("color","red");
+        }
+        else {
+            $("message").text("The input values are correct").css("color","green");
+        }
+
+    }
+        
+
+
+});
